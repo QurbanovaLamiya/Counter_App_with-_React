30deabb2aafc8c1a8453089cb7d257abb438257f
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-10-09 21:39:52 +0400

    Simple_Counter_App_with_React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Component, Fragment } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <Fragment>
+        <h1>hello</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
